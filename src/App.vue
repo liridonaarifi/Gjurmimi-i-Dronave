51c8d45ce59cca53c46d721c9592c5d1383c327f
Petrit <petrit.azizi@smart-bits.ch>
2023-02-13 00:38:34 +0100

    add some styling, update the marker every two seconds, show marker(with drone icon) and draw line.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
 import MapTemplate from './components/MapTemplate.vue'
 </script>
 
@@ -7,7 +6,7 @@ import MapTemplate from './components/MapTemplate.vue'
 
 
 <div class="wrapper">
-      <MapTemplate msg="You did it!" />
+      <MapTemplate msg="Map is shown here!" />
     </div>
 </template>
 
@@ -18,26 +17,11 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
--- src/components/MapTemplate.vue
+++ src/components/MapTemplate.vue
@@ -1,31 +1,61 @@
 <template>
-  <div style="height: 600px; width: 800px">
-    <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+  <div style="height: 100vh; width: 100vw">
+    <l-map ref="map" v-model:zoom="zoom" :center="dronePos">
       <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        layer-type="base"
-        name="OpenStreetMap"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          layer-type="base"
+          name="OpenStreetMap"
       ></l-tile-layer>
+      <l-polyline v-if="$refs.map" ref="path" :lat-lngs="pathCoords"></l-polyline>
+      <l-marker v-if="$refs.map" ref="marker" :icon="droneIcon" :lat-lng="dronePos"></l-marker>
     </l-map>
   </div>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+import L from "leaflet";
+import { LMap, LTileLayer, LMarker, LPolyline } from "@vue-leaflet/vue-leaflet";
+import droneIcon from "../assets/drone.svg"
 
 export default {
   components: {
     LMap,
     LTileLayer,
+    LMarker,
+    LPolyline
   },
   data() {
     return {
-      center: [51.505, -0.09],
-      zoom: 13,
-      
+      center: [47.41322, -1.219482],
+      zoom: 16,
+      dronePos: [47.41322, -1.219482],
+      droneIcon: L.icon({
+        iconUrl: droneIcon,
+        iconSize: [32, 32]
+      }),
+      pathCoords: [[47.41322, -1.219482]]
     };
   },
+  mounted() {
+    this.updateDronePosition();
+  },
+  methods: {
+    updateDronePosition() {
+      this.dronePos = [
+        this.dronePos[0] + Math.random() * 0.0002 - 0.0001,
+        this.dronePos[1] + Math.random() * 0.0002 - 0.0001
+      ];
+      try {
+        debugger
+        this.pathCoords.push(this.dronePos);
+      } catch (error) {
+        console.error("Error updating path coordinates: ", error);
+      }
+      setTimeout(() => this.updateDronePosition(), 2000);
+    }
+  }
+
 };
 </script>
 
