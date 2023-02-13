66a10ea29420851be83861ad470091dcaec3ded8
Petrit <petrit.azizi@smart-bits.ch>
2023-02-13 22:55:55 +0100

    opt

--- src/components/MapTemplate.vue
+++ src/components/MapTemplate.vue
@@ -9,6 +9,7 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { LMap, LTileLayer, LMarker, LPolyline } from "@vue-leaflet/vue-leaflet";
 import droneIcon from "../assets/drone.svg";
+import { initialCoords, latAndLongs, path } from "@/services/locations";
 
 export default {
   components: {
@@ -20,139 +21,51 @@ export default {
   data() {
     return {
       center: [42.654753, 21.164133],
-      zoom: 13,
-      dronePos: [42.654753, 21.164133],
       droneIcon: L.icon({
         iconUrl: droneIcon,
         iconSize: [32, 32],
       }),
-      pathCoords: [
-        [42.654753, 21.164133],
-        [42.653943, 21.163934],
-      ],
 
       map: null,
       marker: null,
       index: 0,
       arr: [],
-      DronIcon: null,
-      array: [
-        [
-          [42.654584, 21.16407],
-          [42.654279, 21.164043],
-        ],
-        [
-          [42.654279, 21.164043],
-          [42.653967, 21.164006],
-        ],
-        [
-          [42.653967, 21.164006],
-          [42.653536, 21.164006],
-        ],
-        [
-          [42.653536, 21.164006],
-          [42.653324, 21.164015],
-        ],
-        [
-          [42.653324, 21.164015],
-          [42.652873, 21.163862],
-        ],
-        [
-          [42.652873, 21.163862],
-          [42.652521, 21.163646],
-        ],
-        [
-          [42.652521, 21.163646],
-          [42.652209, 21.163537],
-        ],
-        [
-          [42.652209, 21.163537],
-          [42.651818, 21.163519],
-        ],
-        [
-          [42.651818, 21.163519],
-          [42.651565, 21.163573],
-        ],
-        [
-          [42.651565, 21.163573],
-          [42.651273, 21.163438],
-        ],
-        [
-          [42.651273, 21.163438],
-          [42.650762, 21.163691],
-        ],
-        [
-          [42.650762, 21.163691],
-          [42.650262, 21.163073],
-        ],
-        [
-          [42.650262, 21.163073],
-          [42.650252, 21.161286],
-        ],
-        [
-          [42.650252, 21.161286],
-          [42.651294, 21.161159],
-        ],
-      ],
+      path: path,
     };
   },
   mounted() {
-    this.map = L.map("map").setView([42.654584, 21.16407], 10);
-    var OpenStreetMap_Mapnik = L.tileLayer(
-      "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      {
-        maxZoom: 17,
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }
+    this.map = L.map("map").setView(latAndLongs[0], 17);
+    const tileLayer = L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+          maxZoom: 17,
+          attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        }
     ).addTo(this.map);
 
-    this.DronIcon = L.icon({
-      iconUrl: droneIcon,
-      iconSize: [30, 30],
-
-    });
-
-    var latlngs = [
-      [42.654584, 21.16407],
-      [42.654584, 21.16407],
-    ];
-    this.marker = L.marker([42.654584, 21.16407], { icon: this.DronIcon }).addTo(
-      this.map
-    );
-    var polyline = L.polyline(latlngs, { color: "red" }).addTo(this.map);
+    this.marker = L.marker(latAndLongs[0], { icon: this.droneIcon }).addTo(this.map);
+    this.polyline = L.polyline(initialCoords, { color: "red" }).addTo(this.map);
+    this.bounds = this.polyline.getBounds();
     this.updateDronePosition();
   },
   methods: {
-    updateDronePosition(indexNumber) {
-      let latlngs = [
-        [42.654584, 21.16407],
-        [42.654279, 21.164043],
-        [42.653967, 21.164006],
-        [42.653536, 21.164006],
-        [42.653324, 21.164015],
-        [42.652873, 21.163862],
-        [42.652521, 21.163646],
-        [42.652209, 21.163537],
-        [42.651818, 21.163519],
-        [42.651565, 21.163573],
-        [42.651273, 21.163438],
-        [42.650762, 21.163691],
-        [42.650262, 21.163073],
-        [42.650252, 21.161286],
-        [42.651294, 21.161159],
-      ];
+    updateDronePosition() {
+      this.arr.push(latAndLongs[this.index]);
+      this.marker.setLatLng(this.path[this.index][0]);
+      this.polyline.setLatLngs(this.arr);
 
-      this.arr.push(latlngs[this.index]);
-      this.marker.setLatLng(this.array[this.index][0]);
-      var polyline = L.polyline(this.arr, { color: "red" }).addTo(this.map);
-      // zoom the map to the polyline
-      this.map.fitBounds(polyline.getBounds());
+      const newBounds = this.polyline.getBounds();
+      if (!this.bounds.equals(newBounds)) {
+        this.bounds = newBounds;
+        this.map.fitBounds(this.bounds);
+      }
+
       this.index++;
 
       setTimeout(() => this.updateDronePosition(), 2000);
-    },
-  },
+    }
+  }
 };
 </script>
 
